81fcf4af8dde460cd5c8acdc053b2e23b04d3be8
zong <Zong20021107@gmail.com>
2026-01-04 01:14:21 +0800

    feat:設定gh-pages自動部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 關鍵：這裡要填入你的 Repo 名稱，前後都要加上斜線
+  base: "/hexRESTfulAPI/",
 })
