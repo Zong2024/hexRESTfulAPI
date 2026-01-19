287bd4fd6055d0066107f2f02a5fa92ad27a67d1
zong <Zong20021107@gmail.com>
2026-01-19 15:35:01 +0800

    feat(產品管理): 實作產品 CRUD 功能並導入管理彈跳視窗

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import ProductModal from "./ProductModal"
 const API_BASE = import.meta.env.VITE_API_BASE_URL
 const API_PATH = import.meta.env.VITE_API_PATH
 
+const defaultModalState = {
+  imageUrl: "",
+  title: "",
+  category: "",
+  unit: "",
+  origin_price: "",
+  price: "",
+  description: "",
+  content: "",
+  is_enabled: 0,
+  imagesUrl: [],
+}
+
 const App = () => {
   const [formData, setFormData] = useState({
-    username: "",
+    username: "love970120@gmail.com",
     password: "",
   })
 
@@ -13,6 +27,16 @@ const App = () => {
   const [products, setProducts] = useState([])
   const [tempProduct, setTempProduct] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [modalData, setModalData] = useState(defaultModalState)
+
+  const [isEditing, setIsEditing] = useState(false)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => {
+    setModalData(defaultModalState)
+    setIsModalOpen(true)
+  }
+  const closeModal = () => setIsModalOpen(false)
 
   const checkSignin = async () => {
     const token = document.cookie
@@ -34,6 +58,7 @@ const App = () => {
       setIsLoading(false)
     }
   }
+
   useEffect(() => {
     checkSignin()
   }, [])
@@ -49,7 +74,7 @@ const App = () => {
       const response = await axios.post(`${API_BASE}/admin/signin`, formData)
       const { token, expired } = response.data
       document.cookie = `hexToken=${token}; expires=${new Date(
-        expired
+        expired,
       )}; path=/`
       axios.defaults.headers.common["Authorization"] = token
       setIsAuth(true)
@@ -58,7 +83,6 @@ const App = () => {
       console.error(error.response?.data || error.message)
     }
   }
-
   const signout = async () => {
     try {
       const response = await axios.post(`${API_BASE}/logout`)
@@ -73,18 +97,48 @@ const App = () => {
       console.log("登出錯誤", error)
     }
   }
-
+  //api
   const fetchProducts = async () => {
     try {
       const url = `${API_BASE}/api/${API_PATH}/admin/products`
       const response = await axios.get(url)
       const { products: data } = response.data
-      return setProducts(data)
+      setProducts(data)
+      console.log(data)
     } catch (error) {
       console.error("商品取得錯誤", error)
     }
   }
+  const addProduct = async rawData => {
+    const productToSend = {
+      ...rawData,
+      origin_price: Number(rawData.origin_price),
+      price: Number(rawData.price),
+      imagesUrl: rawData.imagesUrl.filter(url => url.trim() !== ""),
+    }
+    const response = await axios.post(
+      `${API_BASE}/api/${API_PATH}/admin/product`,
+      {
+        data: productToSend,
+      },
+    )
+    return response.data
+  }
+  const deleteProduct = async id => {
+    const response = await axios.delete(
+      `${API_BASE}/api/${API_PATH}/admin/product/${id}`,
+    )
+    return response.data
+  }
+  const editProduct = async id => {
+    const response = await axios.put(
+      `${API_BASE}/api/${API_PATH}/admin/product/${id}`,
+      { data: tempProduct },
+    )
+    return response.data
+  }
 
+  //event
   const handleSubmit = async e => {
     e.preventDefault()
     await signin()
@@ -92,6 +146,75 @@ const App = () => {
   const handleInputChange = e => {
     const { value, name } = e.target
     setFormData(prev => ({ ...prev, [name]: value }))
+  }
+
+  const formatInputValue = (value, type, checked) => {
+    switch (type) {
+      case "checkbox":
+        return checked ? 1 : 0
+      case "number":
+        return Number(value)
+      default:
+        return value
+    }
+  }
+
+  const handleModalInputChange = e => {
+    const { name, value, type, checked } = e.target
+    setModalData(prev => ({
+      ...prev,
+      [name]: formatInputValue(value, type, checked),
+    }))
+  }
+
+  const handleImageChange = e => {
+    const { value } = e.target
+    setModalData(prev => ({
+      ...prev,
+      imagesUrl: value.split("\n"),
+    }))
+  }
+
+  const handleEditInputChange = e => {
+    const { name, value, type, checked } = e.target
+    setTempProduct(prev => ({
+      ...prev,
+      [name]: formatInputValue(value, type, checked),
+    }))
+  }
+  const handleEditProduct = async id => {
+    try {
+      await editProduct(id)
+      alert("編輯產品成功")
+      fetchProducts()
+    } catch (error) {
+      console.error(error.response?.data || error.message)
+      alert("編輯產品失敗")
+    }
+  }
+
+  const handleAddProduct = async () => {
+    try {
+      await addProduct(modalData)
+      alert("新增產品成功")
+      fetchProducts()
+    } catch (error) {
+      console.error(error.response?.data || error.message)
+      alert("新增產品失敗")
+    } finally {
+      closeModal()
+    }
+  }
+
+  const handleDeleteProduct = async id => {
+    try {
+      await deleteProduct(id)
+      alert("刪除產品成功")
+      fetchProducts()
+    } catch (error) {
+      console.error(error.response?.data || error.message)
+      alert("刪除產品失敗")
+    }
   }
 
   if (isLoading) {
@@ -102,11 +225,18 @@ const App = () => {
     <>
       {isAuth ? (
         <div className='container'>
+          <button type='button' className='btn btn-dark mt-5' onClick={signout}>
+            signout
+          </button>
           <div className='row mt-5'>
             <div className='col-md-6'>
-              <h2>產品列表</h2>
-              <button type='button' onClick={signout}>
-                signout
+              <h2 className='d-inline-block me-3'>產品列表</h2>
+              <button
+                type='button'
+                className='btn btn-warning'
+                onClick={openModal}
+              >
+                新增產品
               </button>
               <table className='table'>
                 <thead>
@@ -116,6 +246,7 @@ const App = () => {
                     <th>售價</th>
                     <th>是否啟用</th>
                     <th>查看細節</th>
+                    <th>刪除</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -134,52 +265,217 @@ const App = () => {
                             查看細節
                           </button>
                         </td>
+                        <td>
+                          <button
+                            type='button'
+                            className='btn btn-danger'
+                            onClick={() => {
+                              handleDeleteProduct(product.id)
+                            }}
+                          >
+                            刪除
+                          </button>
+                        </td>
                       </tr>
                     ))
                   ) : (
                     <tr>
-                      <td colSpan='5'>尚無產品資料</td>
+                      <td colSpan='6'>尚無產品資料</td>
                     </tr>
                   )}
                 </tbody>
               </table>
             </div>
             <div className='col-md-6'>
-              <h2>單一產品細節</h2>
+              <h2 className='text-center'>單一產品細節</h2>
               {tempProduct ? (
-                <div className='card mb-3'>
-                  <img
-                    src={tempProduct.imageUrl}
-                    className='card-img-top primary-image'
-                    alt='主圖'
-                  />
+                <div className='card mb-3 p-4 position-relative'>
+                  {isEditing ? (
+                    <div className='mb-3'>
+                      <label htmlFor='imageUrl' className='form-label'>
+                        主要圖片網址
+                      </label>
+                      <input
+                        type='text'
+                        className='form-control'
+                        id='imageUrl'
+                        name='imageUrl'
+                        value={tempProduct.imageUrl}
+                        onChange={handleEditInputChange}
+                      />
+                    </div>
+                  ) : (
+                    <img
+                      src={tempProduct.imageUrl}
+                      className='card-img-top primary-image'
+                      alt='主圖'
+                    />
+                  )}
+
+                  <button
+                    type='button'
+                    className={`position-absolute top-0 end-0 btn  ${isEditing ? "btn-success" : "btn-primary"}`}
+                    onClick={() => {
+                      if (isEditing) {
+                        handleEditProduct(tempProduct.id)
+                      }
+                      setIsEditing(prev => !prev)
+                    }}
+                  >
+                    {isEditing ? "完成" : "編輯"}
+                  </button>
                   <div className='card-body'>
                     <h5 className='card-title'>
-                      {tempProduct.title}
-                      <span className='badge bg-primary ms-2'>
-                        {tempProduct.category}
-                      </span>
-                    </h5>
-                    <p className='card-text'>
-                      商品描述：{tempProduct.description}
-                    </p>
-                    <p className='card-text'>商品內容：{tempProduct.content}</p>
-                    <div className='d-flex'>
-                      <p className='card-text text-secondary'>
-                        <del>{tempProduct.origin_price}</del>
-                      </p>
-                      元 / {tempProduct.price} 元
-                    </div>
-                    <h5 className='mt-3'>更多圖片：</h5>
-                    <div className='d-flex flex-wrap'>
-                      {tempProduct.imagesUrl?.map((url, index) => (
-                        <img
-                          key={index}
-                          src={url}
-                          className='images'
-                          alt='副圖'
+                      {isEditing ? (
+                        <input
+                          type='text'
+                          className='form-control'
+                          name='title'
+                          value={tempProduct.title}
+                          onChange={handleEditInputChange}
                         />
-                      ))}
+                      ) : (
+                        tempProduct.title
+                      )}
+                    </h5>
+
+                    <div className='mb-3'>
+                      <label htmlFor='description' className='form-label'>
+                        商品描述：
+                      </label>
+                      {isEditing ? (
+                        <textarea
+                          className='form-control'
+                          id='description'
+                          name='description'
+                          rows='3'
+                          value={tempProduct.description}
+                          onChange={handleEditInputChange}
+                        ></textarea>
+                      ) : (
+                        <p className='card-text'>{tempProduct.description}</p>
+                      )}
+                    </div>
+
+                    <div className='mb-3'>
+                      <label htmlFor='content' className='form-label'>
+                        商品內容：
+                      </label>
+                      {isEditing ? (
+                        <textarea
+                          className='form-control'
+                          id='content'
+                          name='content'
+                          rows='3'
+                          value={tempProduct.content}
+                          onChange={handleEditInputChange}
+                        ></textarea>
+                      ) : (
+                        <p className='card-text'>{tempProduct.content}</p>
+                      )}
+                    </div>
+
+                    <div className='row mb-3'>
+                      <div className='col-md-6'>
+                        <label htmlFor='origin_price' className='form-label'>
+                          原價：
+                        </label>
+                        {isEditing ? (
+                          <input
+                            type='number'
+                            className='form-control'
+                            id='origin_price'
+                            name='origin_price'
+                            value={tempProduct.origin_price}
+                            onChange={handleEditInputChange}
+                          />
+                        ) : (
+                          <p className='card-text text-secondary'>
+                            <del>{tempProduct.origin_price}</del> 元
+                          </p>
+                        )}
+                      </div>
+                      <div className='col-md-6'>
+                        <label htmlFor='price' className='form-label'>
+                          售價：
+                        </label>
+                        {isEditing ? (
+                          <input
+                            type='number'
+                            className='form-control'
+                            id='price'
+                            name='price'
+                            value={tempProduct.price}
+                            onChange={handleEditInputChange}
+                          />
+                        ) : (
+                          <p className='card-text'>{tempProduct.price} 元</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className='mb-3'>
+                      <label htmlFor='imagesUrl' className='form-label'>
+                        更多圖片網址 (每行一個)：
+                      </label>
+                      {isEditing ? (
+                        //多圖區 須修改
+                        <textarea
+                          className='form-control'
+                          id='imagesUrl'
+                          name='imagesUrl'
+                          rows='10'
+                          value={tempProduct.imagesUrl}
+                          onChange={e => {
+                            const { value } = e.target
+                            setTempProduct(prev => ({
+                              ...prev,
+                              imagesUrl: value.split("\n"),
+                            }))
+                          }}
+                        ></textarea>
+                      ) : (
+                        <div className='d-flex flex-wrap'>
+                          {tempProduct.imagesUrl?.map((url, index) => (
+                            <img
+                              key={index}
+                              src={url}
+                              className='images'
+                              alt='副圖'
+                            />
+                          ))}
+                        </div>
+                      )}
+                    </div>
+
+                    <div className='mb-3'>
+                      <div className='form-check'>
+                        {isEditing ? (
+                          <input
+                            className='form-check-input'
+                            type='checkbox'
+                            id='is_enabled'
+                            name='is_enabled'
+                            checked={tempProduct.is_enabled === 1}
+                            onChange={handleEditInputChange}
+                          />
+                        ) : (
+                          <input
+                            className='form-check-input'
+                            type='checkbox'
+                            id='is_enabled'
+                            name='is_enabled'
+                            checked={tempProduct.is_enabled === 1}
+                            disabled
+                          />
+                        )}
+                        <label
+                          className='form-check-label'
+                          htmlFor='is_enabled'
+                        >
+                          是否啟用
+                        </label>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -188,6 +484,15 @@ const App = () => {
               )}
             </div>
           </div>
+          {/* 產品 Modal 視窗  */}
+          <ProductModal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            tempProduct={modalData}
+            handleInputChange={handleModalInputChange}
+            handleImageChange={handleImageChange}
+            submitFunction={handleAddProduct}
+          />
         </div>
       ) : (
         <div className='container login'>
